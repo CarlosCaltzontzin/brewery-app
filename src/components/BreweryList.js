dadf07fb85ba7078f906b7faafbaa6d3461bb4c1
Carlos Caltzontzin <cacr71993@yahoo.com>
2024-01-15 10:50:35 -0600

    commit

---
+++ src/components/BreweryList.js
@@ -0,0 +1,25 @@
+// BreweryList.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const BreweryList = () => {
+  const [breweries, setBreweries] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/breweries')
+      .then(response => setBreweries(response.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Breweries</h2>
+      <ul>
+        {breweries.map(brewery => (
+          <li key={brewery.id}>{brewery.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BreweryList;
