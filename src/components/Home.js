d6d1ebcdb4f874a4d3be33c76155705876382556
Carlos Caltzontzin <cacr71993@yahoo.com>
2024-01-15 13:11:49 -0600

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
-// Home.js
 import React from 'react';
 
-const Home = () => (
-  <div>
-    <h1>Breweries</h1>
-    <p>Explore and add your favorite breweries!</p>
-  </div>
-);
+function Home(){
+   return(
+    <div class="wrapper">
+        <h1 align="center">Breweries</h1>
+        <p>Explore and add your favorite breweries!</p>
+    </div>
+   ) 
+}
 
 export default Home;
