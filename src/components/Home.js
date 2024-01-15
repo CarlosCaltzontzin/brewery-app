0b60170e67321bc8bac9a6c6427a51aa6db45d4f
Carlos Caltzontzin <cacr71993@yahoo.com>
2024-01-15 10:52:26 -0600

    commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+// Home.js
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Welcome to Brewery App</h2>
+    <p>Explore and add your favorite breweries!</p>
+  </div>
+);
+
+export default Home;
