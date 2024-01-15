b6768c693507cfb2d2d9df486a6c0488073fecc0
Carlos Caltzontzin <cacr71993@yahoo.com>
2024-01-15 13:28:41 -0600

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return(
+  return (
     <div className="navbar">
-      <NavLink exact to="/" >
+      <NavLink exact to="/" className="nav-link">
         Home
       </NavLink>
-      <NavLink exact to="/breweries" >
+      <NavLink exact to="/breweries" className="nav-link">
         Breweries
       </NavLink>
-      <NavLink exact to="/add" >
+      <NavLink exact to="/add" className="nav-link">
         Add Brewery
       </NavLink>
     </div>
-  )
+  );
 }
 
 export default NavBar;
