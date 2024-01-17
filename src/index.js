9fe14087c34a0c8da6b3e085cff5cf2daed27c3a
Carlos Caltzontzin <cacr71993@yahoo.com>
2024-01-17 17:26:26 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import './index.css';
 
+// Render the App component within the Router
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
+  // Mount the rendered component into the HTML root element with the id "root"
   document.getElementById("root")
 );
