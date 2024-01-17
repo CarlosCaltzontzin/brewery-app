e894db1a02dc511ad5fe089dc761bbc9340ace98
Carlos Caltzontzin <cacr71993@yahoo.com>
2024-01-17 17:16:24 -0600

    commit

--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -3,25 +3,30 @@ import React, { useEffect, useState } from 'react';
 function BreweryList() {
   const [breweries, setBreweries] = useState([]);
 
+  // Fetch request to get the list of breweries and update the state
   useEffect(() => {
     fetch('http://localhost:3001/breweries')
-      .then(response => response.json())
-      .then(data => setBreweries(data))
+    .then(response => response.json())
+    .then(data => setBreweries(data))
   }, []);
-
+  
   return (
     <div class="wrapper">
       <h2 align="center">Breweries</h2>
+      {/* Table to display the list of breweries */}
       <table>
         <thead>
           <tr>
+            {/* Table headers: Name and Address */}
             <th>Name</th>
             <th>Address</th>
           </tr>
         </thead>
         <tbody>
+          {/*Create a row for each brewery */}
           {breweries.map(brewery => (
             <tr key={brewery.id}>
+              {/* Display the name and address of each brewery */}
               <td>{brewery.name}</td>
               <td>{brewery.address}</td>
             </tr>
