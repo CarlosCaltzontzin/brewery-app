ac3b7e0a3c6d088328a4da583ed704791674cd18
Carlos Caltzontzin <cacr71993@yahoo.com>
2024-01-17 17:19:27 -0600

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,16 @@ import React from 'react';
 function Home(){
    return(
     <div align="center" class="wrapper">
+        {/* Heading and subheading */}
         <h1>Breweries</h1>
         <h2>Explore and add your favorite breweries!</h2> 
 
+        {/* Blockquote section with a quote */}
         <div class="blockquote">
             <h1>Beer is proof that God loves us and wants us to be happy</h1>
             <h4>&mdash;Benjamin Franklin</h4>
         </div>
     </div>
-
    ) 
 }
 
